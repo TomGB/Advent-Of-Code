4140b3c5be610cef36077dac73bc9c14b9702aca
Tom Banister <tgbanister@gmail.com>
2025-12-08 07:41:57 +0000

    day08pt1wip

--- 2025/day08/part1.js
+++ 2025/day08/part1.js
@@ -2,13 +2,105 @@ const { debug } = require("../../lib/debug");
 
 const processRawInput = (input) => {
 	const lines = input.trim().split("\n");
-	return lines;
+	return lines.map((line) => line.split(",").map(Number));
 };
 
-const solve = (input) => {
+const solve = (input, productNumber) => {
 	debug("Input:", input);
-	return 0;
+
+	const distances = getAllDistances(input).sort((a, b) => a.distance - b.distance);
+
+	const shortestXConnections = distances.slice(0, productNumber);
+	debug("Shortest distances:", shortestXConnections);
+
+	let circuits = [];
+
+	let circuitId = 0;
+
+	shortestXConnections.map(({ pointA, pointB }) => {
+		const pointAInCircuits = circuits.find(({ point }) => point == pointA);
+		const pointAId = pointAInCircuits?.id;
+		const pointBInCircuits = circuits.find(({ point }) => point == pointB);
+		const pointBId = pointBInCircuits?.id;
+
+		debug(pointAId, pointBId)
+
+		if (pointAInCircuits && pointBInCircuits) {
+			circuits = circuits.map(({ point, id }) => {
+				if (id == pointBId) {
+					return { point, id: pointAId };
+				}
+				return { point, id };
+			});
+			return;
+		}
+		
+		if (pointAInCircuits) {
+			circuits.push({ point: pointB, id: pointAInCircuits.id })
+			return;
+		}
+		
+		if (pointBInCircuits) {
+			circuits.push({ point: pointA, id: pointBInCircuits.id })
+			return;
+		}
+
+		circuitId++;
+		circuits.push({ point: pointA, id: circuitId })
+		circuits.push({ point: pointB, id: circuitId })
+	});
+
+	debug("Circuits:", circuits.length);
+	
+	const numCircuits = circuits.length + input.filter(point => !circuits.find(circuit => circuit.point === point)).length;
+
+	debug("Number of circuits:", numCircuits);
+
+	const circuitSizes = circuits.reduce((acc, { id }) => {
+		if (acc[id]) {
+			acc[id]++;
+		} else {
+			acc[id] = 1;
+		}
+
+		return acc;
+	}, {})
+
+	debug(circuitSizes)
+
+	const productOfTopThree = Object.values(circuitSizes).sort((a, b) => b - a).slice(0,3).reduce((acc, item) => acc * item, 1);
+
+	debug(productOfTopThree);
+
+	return productOfTopThree;
 };
+
+const getDistance = (a, b) => {
+	return Math.sqrt(Math.pow(b[0] - a[0], 2) + Math.pow(b[1] - a[1], 2) + Math.pow(b[2] - a[2], 2));
+}
+
+const getClosestPair = (points) => {
+	return points.reduce((closest, pointA, indexA) => {
+		points.slice(indexA + 1).forEach((pointB) => {
+			const distance = getDistance(pointA, pointB);
+			if (distance < closest.distance) {
+				closest = { pointA, pointB, distance };
+			}
+		});
+		return closest;
+	}, { pointA: null, pointB: null, distance: Infinity });
+}
+
+const getAllDistances = (points) => {
+	const distances = [];
+	points.forEach((pointA, indexA) => {
+		points.slice(indexA + 1).forEach((pointB) => {
+			const distance = getDistance(pointA, pointB);
+			distances.push({ pointA, pointB, distance });
+		});
+	});
+	return distances;
+}
 
 const run = (input) => {
 	const processedInput = processRawInput(input);
@@ -16,4 +108,4 @@ const run = (input) => {
 	return result;
 };
 
-module.exports = { solve, run };
+module.exports = { solve, run, getDistance, processRawInput, getClosestPair };
--- 2025/day08/part1.test.js
+++ 2025/day08/part1.test.js
@@ -1,15 +1,56 @@
-const { solve } = require("./part1");
-
-const testCases = [
-  // TODO: Add test cases
-  // [input, expectedOutput]
-];
+const { solve, getDistance, processRawInput, getClosestPair } = require("./part1");
+const { readInputFile } = require("../../lib/input");
 
 describe("Part 1", () => {
-  testCases.forEach(([input, expectedOutput], idx) => {
-    test(`case ${idx + 1}`, () => {
-      const result = solve(input);
-      expect(result).toBe(expectedOutput);
-    });
+  test("JS array behaviour", () => {
+    const array = [{ id: 1 }, { id: 2},{ id: 3}];
+
+    const one = array.find(({id}) => id == 1)?.id;
+
+    console.log(one)
+
+    array[0].id=6;
+
+    console.log(one);
+  })
+  test("In this example, the two junction boxes which are closest together are 162,817,812 and 425,690,689", () => {
+
+    const testInput = readInputFile("testinput.txt");
+    const input = processRawInput(testInput);
+
+    expect(getClosestPair(input)).toMatchObject({ pointA: [162, 817, 812], pointB: [425, 690, 689] });
+
+  })
+  test("After connecting 10 circuits the product of the 3 largest circuits should be 40", () => {
+
+  })
+});
+
+describe("getDistance", () => {
+  
+  test("should return 0 for identical coordinates", () => {
+    expect(getDistance([0, 0, 0], [0, 0, 0])).toBe(0);
+  });
+
+  test("for simple 3-4-5 triangle", () => {
+    expect(getDistance([0, 0, 0], [3, 4, 0])).toBeCloseTo(5, 2);
+  });
+
+  test("between 162,817,812 and 425,690,689 should be 316.90", () => {
+    expect(getDistance([162, 817, 812], [425, 690, 689])).toBeCloseTo(316.90, 2);
+  });
+
+  test("with large coordinates in hundreds", () => {
+    expect(getDistance([0, 0, 0], [300, 400, 0])).toBeCloseTo(500, 2);
+  });
+
+  test("with large negative coordinates", () => {
+    expect(getDistance([-500, -600, -700], [500, 600, 700])).toBeCloseTo(2097.62, 2);
+  });
+
+  test("should be symmetric", () => {
+    const p1 = [100, 200, 300];
+    const p2 = [400, 500, 600];
+    expect(getDistance(p1, p2)).toBeCloseTo(getDistance(p2, p1), 5);
   });
 });
