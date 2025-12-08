a26154aebb8cefb59cce6fd639618685a66d07e8
Tom Banister <tgbanister@gmail.com>
2025-12-08 11:31:16 +0000

    day08 part 1

--- 2025/day08/part1.js
+++ 2025/day08/part1.js
@@ -6,15 +6,11 @@ const processRawInput = (input) => {
 };
 
 const solve = (input, productNumber) => {
-	debug("Input:", input);
-
 	const distances = getAllDistances(input).sort((a, b) => a.distance - b.distance);
 
 	const shortestXConnections = distances.slice(0, productNumber);
-	debug("Shortest distances:", shortestXConnections);
 
 	let circuits = [];
-
 	let circuitId = 0;
 
 	shortestXConnections.map(({ pointA, pointB }) => {
@@ -22,8 +18,6 @@ const solve = (input, productNumber) => {
 		const pointAId = pointAInCircuits?.id;
 		const pointBInCircuits = circuits.find(({ point }) => point == pointB);
 		const pointBId = pointBInCircuits?.id;
-
-		debug(pointAId, pointBId)
 
 		if (pointAInCircuits && pointBInCircuits) {
 			circuits = circuits.map(({ point, id }) => {
@@ -50,12 +44,6 @@ const solve = (input, productNumber) => {
 		circuits.push({ point: pointB, id: circuitId })
 	});
 
-	debug("Circuits:", circuits.length);
-	
-	const numCircuits = circuits.length + input.filter(point => !circuits.find(circuit => circuit.point === point)).length;
-
-	debug("Number of circuits:", numCircuits);
-
 	const circuitSizes = circuits.reduce((acc, { id }) => {
 		if (acc[id]) {
 			acc[id]++;
@@ -66,29 +54,13 @@ const solve = (input, productNumber) => {
 		return acc;
 	}, {})
 
-	debug(circuitSizes)
-
 	const productOfTopThree = Object.values(circuitSizes).sort((a, b) => b - a).slice(0,3).reduce((acc, item) => acc * item, 1);
-
-	debug(productOfTopThree);
 
 	return productOfTopThree;
 };
 
 const getDistance = (a, b) => {
 	return Math.sqrt(Math.pow(b[0] - a[0], 2) + Math.pow(b[1] - a[1], 2) + Math.pow(b[2] - a[2], 2));
-}
-
-const getClosestPair = (points) => {
-	return points.reduce((closest, pointA, indexA) => {
-		points.slice(indexA + 1).forEach((pointB) => {
-			const distance = getDistance(pointA, pointB);
-			if (distance < closest.distance) {
-				closest = { pointA, pointB, distance };
-			}
-		});
-		return closest;
-	}, { pointA: null, pointB: null, distance: Infinity });
 }
 
 const getAllDistances = (points) => {
@@ -102,10 +74,10 @@ const getAllDistances = (points) => {
 	return distances;
 }
 
-const run = (input) => {
+const run = (input, productNumber) => {
 	const processedInput = processRawInput(input);
-	const result = solve(processedInput);
+	const result = solve(processedInput, productNumber);
 	return result;
 };
 
-module.exports = { solve, run, getDistance, processRawInput, getClosestPair };
+module.exports = { solve, run, getDistance, processRawInput };
