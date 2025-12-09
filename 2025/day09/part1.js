9c0aef36c1c99da4046caed94aa76141bf236029
Tom Banister <tgbanister@gmail.com>
2025-12-09 15:14:11 +0000

    day09

---
+++ 2025/day09/part1.js
@@ -0,0 +1,19 @@
+const { debug } = require("../../lib/debug");
+
+const processRawInput = (input) => {
+	const lines = input.trim().split("\n");
+	return lines;
+};
+
+const solve = (input) => {
+	debug("Input:", input);
+	return 0;
+};
+
+const run = (input) => {
+	const processedInput = processRawInput(input);
+	const result = solve(processedInput);
+	return result;
+};
+
+module.exports = { solve, run };
